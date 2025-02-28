1473fe29835f3a5a6963e599f7e03246ed6c2ab1
Lee Lott <llott88@gmail.com>
2025-02-27 20:17:55 -0600

    #22 - Added MediatR support for file decryption.

--- Utilities.Gpg.Tests/MediaRTests.cs
+++ Utilities.Gpg.Tests/MediaRTests.cs
@@ -20,4 +20,23 @@ public class MediaRTests
 		mock.Verify(g => g.EncryptFileAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
 		mock.VerifyNoOtherCalls();
 	}
+
+	[Fact]
+	public void DecryptFileHandler_CallsDecryptFileAsync()
+	{
+		//Arrange
+		Mock<IGpg> mock = new();
+		DecryptFileCommand request = new("inputFileLocation", "outputFileLocation", "privateKeyName", "privateKeyPassword");
+		DecryptFileCommandHandler handler = new(mock.Object);
+
+		//Act
+		_ = handler.Handle(request, CancellationToken.None);
+
+		//Assert
+		mock.Verify(g => g.DecryptFileAsync(It.Is<string>(s => s == "inputFileLocation"), 
+			It.Is<string>(s => s == "outputFileLocation"), 
+			It.Is<string>(s => s == "privateKeyName"), 
+			It.Is<string>(s => s == "privateKeyPassword")), Times.Once);
+		mock.VerifyNoOtherCalls();
+	}
 }
